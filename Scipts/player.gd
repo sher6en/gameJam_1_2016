5449a34b2a846640b6029d349aa3cabc551da73b
sher6en <59540064+sher6en@users.noreply.github.com>
2026-01-10 15:13:19 +0200

    Added path finding, a bit drunk

--- Scipts/player.gd
+++ Scipts/player.gd
@@ -1,30 +1,43 @@
 extends CharacterBody2D
 
-
 const SPEED = 10.0
 
-@onready var target_position = $"../supp".position
+@onready var current_target_position = Vector2i(0,0)
+var current_path = []
+var current_path_idx = 0
+func _physics_process(delta: float) -> void:
+	
+	var target = get_parent().get_child(-1)
+	if current_target_position != Vector2i(target.position): #A new target was created
+		print("Old target position ", current_target_position)
+		current_target_position = Vector2i(target.position)
+		current_path = []
+		current_path_idx = 0
+		print("New target position ", current_target_position)
+		
+		var position_scaled16 = Vector2i(round(position/16))
+		var target_position_scaled16 = Vector2i(round(current_target_position/16))
+		
+		print("ort pos s16: ", position_scaled16, " target pos s16:", target_position_scaled16)
+		var current_path_block = position_scaled16
+		while (current_path_block.x != target_position_scaled16.x):
+			current_path.push_back(16*current_path_block+Vector2i(8,0))
+			current_path_block.x = move_toward(current_path_block.x, target_position_scaled16.x, 1)
+			print(current_path, current_path_block/8)
+		while (current_path_block.y != target_position_scaled16.y):
+			current_path_block.y = move_toward(current_path_block.y, target_position_scaled16.y, 1)
+			current_path.push_back(16*current_path_block)
+			print(current_path, current_path_block/8+Vector2i(8,0))
+		current_path.push_back(current_target_position+Vector2i(8,0))
+	
+	if len(current_path) != 0:	
+		position.x = move_toward(position.x, current_path[0].x, SPEED*delta)
+		position.y = move_toward(position.y, current_path[0].y, SPEED*delta)
 
-func _physics_process(delta: float) -> void: 
-	var astar_grid = AStarGrid2D.new()
-	astar_grid.region = Rect2i(0, 0, 18, 10)
-	astar_grid.cell_size = Vector2(16,16)
-	astar_grid.update()
-	var path = astar_grid.get_id_path(Vector2i(position/16), Vector2i(target_position/16))
+		if position == Vector2(current_path[0]):
+			print("got to step, popping")
+			current_path.pop_front()
+			print("current path ", current_path)
+
 		
-	#print("Ort pos: ", position, " target pos: ", target_position, " path: ", path)
-		
-	if len(path) > 0:
-		if Vector2i(position)==16*path[0]:
-			position.x = move_toward(position.x, 16*path[1].x, SPEED*delta)
-			position.y = move_toward(position.y, 16*path[1].y, SPEED*delta)
-		else:
-			position.x = move_toward(position.x, 16*path[0].x, SPEED*delta)
-			position.y = move_toward(position.y, 16*path[0].y, SPEED*delta)
-		
-		#print("path targetpos", Vector2i(position)/16, path[path_idx]/16, target_position/16, "\n", path, "\n")
-		#position.x = move_toward(position.x, 16*path[path_idx].x, SPEED*delta)
-		#position.y = move_toward(position.y, 16*path[path_idx].y, SPEED*delta)
-	
-	
 	move_and_slide()
