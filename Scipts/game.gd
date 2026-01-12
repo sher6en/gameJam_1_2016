b1dbc1760213550166bd5180100ff4aca740427b
ronwnor <ron123online@gmail.com>
2026-01-12 13:19:17 +0200

    refactored target gen

--- Scipts/game.gd
+++ Scipts/game.gd
@@ -13,44 +13,11 @@ var battery_spawn_timer := MAX_BATTERY_SPAWN_TIMER
 var target_instance = null
 @onready var battery: Resource = preload("res://Scenes/battery.tscn")
 
-
-# generates a random integer point on the perimeter of a rectangle defined by 2 points
-#  
-func get_random_point(p1: Vector2i, p2: Vector2i) -> Vector2i:
-	var side := randi_range(1, 4)
-	
-	var random_point := Vector2i.ZERO
-	
-	if side == 1:
-		random_point.x = p1.x
-		random_point.y = randi_range(p1.y+16, p2.y-16)
-	elif side == 2:
-		random_point.x = p2.x
-		random_point.y = randi_range(p1.y+16, p2.y-16)
-	elif side == 3:
-		random_point.y = p1.y
-		random_point.x = randi_range(p1.x+16, p2.x-16)
-	elif side == 4: 
-		random_point.y = p2.y
-		random_point.x = randi_range(p1.x+16, p2.x-16)
-
-	# snap to multiples of 16 using integer division
-	random_point /= 16
-	random_point *= 16
-	
-	print(random_point)
-	
-	random_point.x -= 1
-	return random_point  
 	
 func _ready() -> void:
 	
 	target_instance = target.instantiate()
 	add_child(target_instance)
-	
-	var spawn_loc: Vector2i = get_random_point(point_1, point_2) 
-	target_instance.set_position(spawn_loc)
-	
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scipts/target.gd
+++ Scipts/target.gd
@@ -1,52 +1,57 @@
 extends Area2D
 
-static var point_1: Vector2i = Vector2i(64,32)
-static var point_2: Vector2i = Vector2i(239,143)
-var outside: int = 0
+static var p1 := Vector2i(64,32)
+static var p2 := Vector2i(239,143)
+var current_side := randi_range(1, 4)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	randomize_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-func get_random_point(p1: Vector2i, p2: Vector2i, side) -> Vector2i:
-	var return_value: Vector2i = Vector2i(0,0)
-
-
-	#var rand_x: int
+# generates a random integer point on the given side of a rectangle defined by 2 points
+func randomize_position() -> void:
 	
-	var random_point = Vector2i.ZERO
+	var side = randi_range(1, 4)
+	while(side == current_side):
+		side = randi_range(1, 4)
+	current_side = side
+	
+	var random_point := Vector2i.ZERO
 	
 	if side == 1:
-		random_point.x = p1.x/16
-		random_point.y = randi_range(p1.y+16, p2.y-16)/16
+		random_point.x = p1.x
+		random_point.y = randi_range(p1.y+16, p2.y-16)
 	elif side == 2:
-		random_point.x = p2.x/16
-		random_point.y = randi_range(p1.y+16, p2.y-16)/16
+		random_point.x = p2.x
+		random_point.y = randi_range(p1.y+16, p2.y-16)
 	elif side == 3:
-		random_point.y = p1.y/16
-		random_point.x = randi_range(p1.x+16, p2.x-16)/16
+		random_point.y = p1.y
+		random_point.x = randi_range(p1.x+16, p2.x-16)
 	elif side == 4: 
-		random_point.y = p2.y/16
-		random_point.x = randi_range(p1.x+16, p2.x-16)/16
-	
-  
+		random_point.y = p2.y
+		random_point.x = randi_range(p1.x+16, p2.x-16)
+
+	# snap to multiples of 16 using integer division
+	random_point /= 16
 	random_point *= 16
+	
+	print(random_point)
+	
 	random_point.x -= 1
-	return random_point  	
+	position = random_point  
 
 func _on_area_entered(area: Area2D) -> void:
+	
+	# if ort touches the target it's game over
 	if area.get_parent().name == "ort":
-		print("ort touch target")
-		
 		get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
-	var random_num = randi_range(1, 4)
-	while(random_num == outside):
-		random_num = randi_range(1, 4)
-	outside = random_num
-	var spawn_loc: Vector2i = get_random_point(point_1, point_2, outside)
-	self.set_position(spawn_loc)
-	#pass # Replace with function body.
+		return
+	
+	# otherwise move the target to a random location on another side 
+
+	randomize_position()
+	#self.set_position(spawn_loc)
